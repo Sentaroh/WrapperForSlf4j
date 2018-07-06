e73ec9d0ad708c90d8f9bcc340355f552d562e41
sentaroh <gm.developer.fhoshino@gmail.com>
2018-07-07 03:22:57 +0900

    Initial Commit

---
+++ src/org/slf4j/Logger.java
@@ -0,0 +1,162 @@
+package org.slf4j;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Logger {
+
+	private String mClassName="";
+//	private static LoggerOption mLoggerOption=null;
+	private StringBuilder log_builder=new StringBuilder(1024);
+	public Logger(Class log_class, LoggerOption log_opt) {
+		mClassName=log_class.getName();
+//		mLoggerOption=log_opt;
+	}
+	
+	private void putLogMsg(Exception e, String ...msg) {
+		log_builder.setLength(0);
+		if (LoggerOption.appendTime) {
+			log_builder.append(convDateTime(System.currentTimeMillis()));
+			log_builder.append(" ");
+		}
+		for(String m:msg) log_builder.append(m).append(" ");
+		if (e!=null) {
+//			log_builder.append(e.getMessage());
+			StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter( stringWriter );
+            e.printStackTrace( printWriter );
+            log_builder.append("\n").append(stringWriter.toString());
+		}
+		System.out.println(log_builder.toString());
+	}
+	
+	public void info(String msg, Exception e) {
+		if (isInfoEnabled()) {
+			putLogMsg(e, "[Info ]", mClassName, msg);
+		}
+	}
+	
+	public void debug(String msg, Exception e) {
+		if (isDebugEnabled()) putLogMsg(e, "[Debug]", mClassName,msg);
+	}
+	public void warn(String msg, Exception e) {
+		if (isWarnEnabled()) putLogMsg(e, mClassName,"[Warn ]",msg);
+	}
+	public void trace(String msg, Exception e) {
+		if (isTraceEnabled()) putLogMsg(e, "[Trace]",mClassName,msg);
+	}
+
+	public void error(String msg, Exception e) {
+		if (isErrorEnabled()) putLogMsg(e, "[Error]",mClassName,msg);
+	}
+
+	public void info(String msg) {
+		if (isInfoEnabled()) info(msg,null);
+	}
+	public void debug(String msg) {
+		if (isDebugEnabled()) debug(msg,null);
+	}
+	public void warn(String msg) {
+		if (isWarnEnabled())warn(msg,null);
+	}
+	public void trace(String msg) {
+		if (isTraceEnabled()) trace(msg,null);
+	}
+
+	public void error(String msg) {
+		if (isErrorEnabled())error(msg, null);
+	}
+	
+	public boolean isDebugEnabled() {
+		return LoggerOption.debugEnabled;
+	}
+
+	public boolean isInfoEnabled() {
+		return LoggerOption.infoEnabled;
+	}
+	public boolean isErrorEnabled() {
+		return LoggerOption.errorEnabled;
+	}
+
+	public boolean isTraceEnabled() {
+		return LoggerOption.traceEnabled;
+	}
+
+	public boolean isWarnEnabled() {
+		return LoggerOption.warnEnabled;
+	}
+
+    final static private String convDateTime(long time) {
+    	final Calendar gcal = new GregorianCalendar();
+    	gcal.setTimeInMillis(time);
+    	final int yyyy=gcal.get(Calendar.YEAR);
+    	final int month=gcal.get(Calendar.MONTH)+1;
+    	final int day=gcal.get(Calendar.DATE);
+    	final int hours=gcal.get(Calendar.HOUR_OF_DAY);
+    	final int minutes=gcal.get(Calendar.MINUTE);
+    	final int second=gcal.get(Calendar.SECOND);
+    	final int ms=gcal.get(Calendar.MILLISECOND);
+    	
+    	final String s_yyyy=String.valueOf(yyyy);
+    	final String s_month=String.valueOf(month);
+    	final String s_day=String.valueOf(day);
+    	final String s_hours=String.valueOf(hours);
+    	final String s_minutes=String.valueOf(minutes);
+    	final String s_second=String.valueOf(second);
+    	final String s_ms=String.valueOf(ms);
+    	
+    	final StringBuilder sb=new StringBuilder(64)
+    		.append(s_yyyy).append("/");
+    	
+    	if (month>9) sb.append(s_month);
+    	else sb.append("0").append(s_month);
+    	sb.append("/");
+    	
+    	if (day>9) sb.append(s_day);
+    	else sb.append("0").append(s_day);
+    	sb.append(" ");
+    	
+    	if (hours>9) sb.append(s_hours);
+    	else sb.append("0").append(s_hours);
+    	sb.append(":");
+    	
+    	if (minutes>9) sb.append(s_minutes);
+    	else sb.append("0").append(s_minutes);
+    	sb.append(":");
+
+    	if (second>9) sb.append(s_second);
+    	else sb.append("0").append(s_second);
+    	sb.append(".");
+    	
+    	if (ms>99) sb.append(s_ms);
+    	else {
+    		if (ms>9) sb.append("0").append(s_ms);
+    		else sb.append("00").append(s_ms);
+    	}
+    	return sb.toString();
+    };
+
+	public void setAppendTime(boolean append) {
+		LoggerOption.appendTime=append;
+	}
+
+	/**
+	   * Set logger option
+	   *
+	   * @param debug true is enable debug
+	   * @param error true is enable error
+	   * @param info true is enable info
+	   * @param trace true is enable trace
+	   * @param warn true is enable warn
+	   */
+	public void setLogOption(boolean debug, boolean error, boolean info, boolean trace, boolean warn) {
+		LoggerOption.debugEnabled=debug;
+		LoggerOption.errorEnabled=error;
+		LoggerOption.infoEnabled=info;
+		LoggerOption.traceEnabled=trace;
+		LoggerOption.warnEnabled=warn;
+	}
+
+}
---
+++ src/org/slf4j/LoggerFactory.java
@@ -0,0 +1,17 @@
+package org.slf4j;
+
+public class LoggerFactory {
+	private static LoggerOption logger_option=new LoggerOption();
+	public static Logger getLogger(Class log_class) {
+		return new Logger(log_class, logger_option);
+	}
+}
+class LoggerOption {
+	static public boolean debugEnabled=false;
+	static public boolean errorEnabled=false;
+	static public boolean infoEnabled=false;
+	static public boolean traceEnabled=false; 
+	static public boolean warnEnabled=false;
+	static public boolean appendTime=false;
+}
+
